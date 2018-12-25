602dd64f3622667d75976a545b18fd976b0ec42a
adnan <adnan@habbat.ml>
2018-12-25 15:55:22 +0100

    Initial commit

--- Back-end/Weather_app/Date/tests.py
+++ Back-end/Weather_app/Date/tests.py
@@ -1,3 +1,16 @@
-from django.test import TestCase
+# from django.test import TestCase
+from django.test import SimpleTestCase # cause we are not using a database
 
-# Create your tests here.
+
+
+class SimpleTests(SimpleTestCase):
+  def test_index_page_status_code(self):
+    response = self.client.get('/')
+    self.assertEqual(response.status_code, 200)
+
+  # def test_about_page_status_code(self):
+  #   response = self.client.get('/about/')
+  #   self.assertEqual(response.status_code, 200)
+
+
+
