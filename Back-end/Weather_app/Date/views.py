85093d3061afeaa3de4a586a7da6ec0828590e11
adnan <adnan@habbat.ml>
2018-12-25 13:53:31 +0100

    Initial commit

--- Back-end/Weather_app/Date/views.py
+++ Back-end/Weather_app/Date/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-
-# Create your views here.
+from django.views.generic import TemplateView
 
 
-def homePageView(request):
-  return HttpResponse('Hello, World!')
+class HomePageView(TemplateView):
+  template_name = "index.html"
+  
+
+
+
