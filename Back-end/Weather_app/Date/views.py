43c3177eccb62604663bb3479dca055e2e6ddb3e
adnan <adnan@habbat.ml>
2018-12-29 21:39:42 +0100

    Initial commit after cleaning up code

--- Back-end/Weather_app/Date/views.py
+++ Back-end/Weather_app/Date/views.py
@@ -5,83 +5,76 @@ import json
 import math
 import datetime
 
+
 ls = ["Mon","Tue","Wed","Thu","Fri","Sat","Sun"]
-def fer_to_cel(value):
-  result = (value - 32)/1.8000
-  return round(result)
-def mph_to_kph(value):
-  result = value *  1.609344
-  return round(float("%.2f" % result))
+
+
+def weather_api(lat, lon, epoch_time):
+  dark_sky_URL = "https://api.darksky.net/forecast/becbbc15c724474fdab20f1d500ac33b/{0},{1},{2}?units=si".format(lat, lon, epoch_time)
+  response = requests.get(dark_sky_URL)
+  weather_api_data = response.json()
+  return weather_api_data
+
 def epoch_time():
-  return datetime.datetime.now().timestamp()
-def epoch_to_human(value):
+  return int(datetime.datetime.now().timestamp())
+
+def epoch_time_to_human(value):
     result = '' + datetime.datetime.fromtimestamp(value).strftime('%A')[:3]
     result += ', ' + datetime.datetime.fromtimestamp(value).strftime('%d')
     result += ' ' + datetime.datetime.fromtimestamp(value).strftime('%B')
     result += ' ' + datetime.datetime.fromtimestamp(value).strftime('%Y')
     return result
-def epoch_today_name(ts):
-  three_letter = datetime.datetime.fromtimestamp(ts).strftime('%A')[:3]
-  return three_letter
+
+def epoch_time_day_name(value):
+    day_name = datetime.datetime.fromtimestamp(value).strftime('%A')[:3]
+    return day_name
 
 def get_location():
-  headers = {
-    'Accept': 'application/json'
-  }
+  headers = {'Accept': 'application/json'}
   KEY = '3f55b07b200471d3ee97ce323483420208527f68fb6d3f0e57df3db5'
-  response = requests.get('https://api.ipdata.co?api-key={}'.format(KEY), headers=headers)
-
-  response_body = response.json()
+  resp = requests.get('https://api.ipdata.co?api-key={}'.format(KEY), headers=headers)
+  response_body = resp.json()
   latitude = response_body['latitude']
   longitude = response_body['longitude']
   city = response_body['city']
   return latitude, longitude, city
-def data():
-    API_Secret_key = "becbbc15c724474fdab20f1d500ac33b"
-    API_URL = "https://api.darksky.net/forecast/{0}/{1},{2},{3}"
-    resp = requests.get(API_URL.format(API_Secret_key, get_location()[0], get_location()[1], int(epoch_time())))
-    data = resp.json()
-    return data
-print(epoch_today_name(epoch_time()+86400))
+
 def index(request):
     # TIME
-    current_time = data()["currently"]['time']
-    current_time_human = epoch_to_human(current_time)
-    # CURRENT TEMPERATURE  °C & °F MORE PRECISE
-    current_temp_inF = round(data()["currently"]["temperature"])
-    current_temp_inC = fer_to_cel(current_temp_inF)
-    # SUMMARY SHORT
-    current_summary = data()["currently"]["summary"]
-    # WIND SPEED MPH KPH()
-    current_wind_speed_mph = data()["currently"]["windSpeed"]
-    current_wind_speed_kph = mph_to_kph(data()["currently"]["windSpeed"])
+    current_time = epoch_time()
+    current_time_human = epoch_time_to_human(current_time)
+    # CURRENT TEMPERATURE  °C MORE PRECISE
+    current_temp_inC = round(weather_api(get_location()[0], get_location()[1], epoch_time())["currently"]["temperature"])
+    # CURRENT SUMMARY (SHORT)
+    current_summary = weather_api(get_location()[0], get_location()[1], epoch_time())["currently"]["summary"]
+    # CURRENT WIND SPEED Meters per second.
+    current_wind_speed_mps = weather_api(get_location()[0], get_location()[1], epoch_time())["currently"]["windSpeed"]
+    current_wind_speed_kph = round(current_wind_speed_mps * 3.6)
     # SUMMARY EXTENDED
-    summary = data()["daily"]["data"][0]["summary"]
-    # DAILY HIGH TEMPERATURE AND LOW TEMPERATURE °C & °F
-    temp_high_inF = round(data()["daily"]["data"][0]["temperatureHigh"])
-    temp_high_inC = fer_to_cel(temp_high_inF)
-    temp_low_inF = round(data()["daily"]["data"][0]["temperatureLow"])
-    temp_low_inC = fer_to_cel(temp_low_inF)
+    summary = weather_api(get_location()[0], get_location()[1], epoch_time())["daily"]["data"][0]["summary"]
+    # DAILY HIGH TEMPERATURE AND LOW TEMPERATURE °C
+    temp_high_inC = round(weather_api(get_location()[0], get_location()[1], epoch_time())["daily"]["data"][0]["temperatureHigh"])
+    temp_low_inC = round(weather_api(get_location()[0], get_location()[1], epoch_time())["daily"]["data"][0]["temperatureLow"])
     # ICON OF THE CURRENT TEMP
-    icon = data()["currently"]["icon"]
+    icon = weather_api(get_location()[0], get_location()[1], epoch_time())["currently"]["icon"]
     if '-' in icon:
       icon = ''.join(icon.split('-')[:-1])
     else:
-      icon      
+      icon  
     weather = {
       'time': current_time_human,
       'city': get_location()[2],
-      'temp_F': current_temp_inF,
       'temp_C': current_temp_inC,
       'wind_kph': current_wind_speed_kph,
-      'wind_mph': current_wind_speed_mph,
       'summary_short': current_summary,
       'summary_extended': summary,
-      'temp_high_F': temp_high_inF,
-      'temp_low_F': temp_low_inF,
       'temp_high_C': temp_high_inC,
       'temp_low_C': temp_low_inC,
       'icon': icon,
+      'day_name': epoch_time_day_name(epoch_time()),
+      'weekdays': ls,
     }
     context = {'weather' : weather}
     return render(request, 'Date/index.html', context) #returns the index.html template
+
+print(ls.index(epoch_time_day_name(epoch_time())))
