2df44953ef44371db98b9e1ecea258fb254f947a
Adnan Habbat <adnan@habbat.ml>
2019-01-06 00:58:48 +0100

    This will handel the city input

--- Back-end/Weather_app/Date/forms.py
+++ Back-end/Weather_app/Date/forms.py
@@ -2,8 +2,4 @@ from django import forms
 
 
 class CityForm(forms.Form):
-    city_name = forms.CharField(label='', max_length=12)
-
-
-# class NameForm(forms.Form):
-#     your_name = forms.CharField(label='Your name', max_length=100)
+    city_name = forms.CharField(label='', max_length=12, widget=forms.TextInput(attrs={'autocomplete':'off', 'placeholder':'City Name'}))
