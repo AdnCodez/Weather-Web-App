4ee185505c52e47cc7b01c174247727a974b587b
Adnan Habbat <adnan.7.habbat@gmail.com>
2019-01-05 23:57:24 +0000

    Connect to the cities db and get lat and long

---
+++ Back-end/Weather_app/Date/city.py
@@ -0,0 +1,22 @@
+import sqlite3
+import os.path
+
+
+def fun(city_name):
+  ls = []
+  BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+  db_path = os.path.join(BASE_DIR, "cities.db")
+  with sqlite3.connect(db_path) as db:
+
+      cursor = db.cursor()
+    
+      var = cursor.execute("""select * from worldcities WHERE Country == 'ma' AND City == '{}'""".format(city_name))
+      for row in var:
+        ls.append(row[0])
+        ls.append(row[1])
+        ls.append(row[5])
+        ls.append(row[6])
+  return ls
+
+
+print(fun('khouribga'))
