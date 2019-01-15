a66334c4a2c76506ac6a89269cedd0a70f60a434
Adnan Habbat <adnan.7.habbat@gmail.com>
2019-01-15 12:11:13 +0100

    Initial commit of the script that gets data from db

--- Back-end/Weather_app/Date/city.py
+++ Back-end/Weather_app/Date/city.py
@@ -2,21 +2,23 @@ import sqlite3
 import os.path
 
 
+# ls = []
+city_lat = 0
+city_long = 0
 def fun(city_name):
-  ls = []
-  BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-  db_path = os.path.join(BASE_DIR, "cities.db")
-  with sqlite3.connect(db_path) as db:
+    BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+    db_path = os.path.join(BASE_DIR, "weatherDB.db")
+    with sqlite3.connect(db_path) as db:
+        global city_lat
+        global city_long
+        cursor = db.cursor()
+        # print(cursor)
+        # var = cursor.execute("""select * from worldcities WHERE Country == 'ma' AND City == '{}'""".format(city_name))
+        var = cursor.execute("select * from popcitiesonly WHERE City == '{}'".format(city_name))
+        for row in var:
+          city_lat  = row[5]
+          city_long = row[6]
+    return city_lat, city_long
 
-      cursor = db.cursor()
-    
-      var = cursor.execute("""select * from worldcities WHERE Country == 'ma' AND City == '{}'""".format(city_name))
-      for row in var:
-        ls.append(row[0])
-        ls.append(row[1])
-        ls.append(row[5])
-        ls.append(row[6])
-  return ls
 
-
-print(fun('rabat'))
+# print(float(fun('kenitra')[0]))
